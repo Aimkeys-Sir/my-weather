8c324ca66cde79b8ec65b2c6a82a183dfec64fff
Aimkeys Kelvin Mwaura <kelvin.ngugi@student.moringaschool.com>
2022-07-19 11:40:07 +0300

    components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
+import WeatherData from './components/WeatherData';
+import NavBar from './components/NavBar';
+import Clouds from './components/Clouds'
+import AirQuality from './components/AirQuality';
+import ForeCast from './components/Forecast';
 
 function App() {
+  const [data, setData] = useState({})
+  useEffect(() => {
+    fetch(`http://api.weatherapi.com/v1/forecast.json?key=b4bb0f58f9a64179ac1103527221807&q=Nairobi&aqi=yes`)
+      .then(res => res.json())
+      .then(res => setData(res))
+  }, [])
+  let time = data.forecast ? data.current.last_updated_epoch : ""
+  // time=time.substr(time.length-5,time.length)
+  console.log(data)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ width: "66.7%" }}>
+        <NavBar />
+        <div style={{ display: "flex" }}>
+          <Clouds />
+          <AirQuality />
+        </div>
+      </div>
+      {data.forecast ? <ForeCast forecastArr={data.forecast.forecastday[0].hour.filter(item => item.time_epoch > time)} /> : null}
     </div>
   );
 }
