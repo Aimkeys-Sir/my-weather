0ab6e4d4fb1f8feee9a49e71e261f65237b3080f
Aimkeys Kelvin Mwaura <kelvin.ngugi@student.moringaschool.com>
2022-07-29 15:43:22 +0300

    added search and hamburger collapse

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react";
 import "./App.css";
-import WeatherData from "./components/WeatherData";
 import NavBar from "./components/NavBar";
 import Clouds from "./components/Clouds";
 import AirQuality from "./components/AirQuality";
@@ -8,30 +7,38 @@ import ForeCast from "./components/Forecast";
 import SunMoon from "./components/SunMoon";
 import Visibility from "./components/Visibility";
 import Temperature from "./components/Temperature";
-import SunUv from "./components/SunUv";
 import Wind from "./components/Wind";
 import Settings from "./components/Settings";
 
 function App() {
+  const [alerts,setAlerts]=useState()
+  const [place,setPlace]=useState("Nairobi")
   const [data, setData] = useState({})
+  const [showSideBar,setShowSideBar]=useState(false)
   useEffect(() => {
-    fetch(`http://api.weatherapi.com/v1/forecast.json?key=b4bb0f58f9a64179ac1103527221807&q=Nairobi&aqi=yes`)
+    fetch(`http://api.weatherapi.com/v1/forecast.json?key=b4bb0f58f9a64179ac1103527221807&q=${place}&aqi=yes`)
       .then((res) => res.json())
       .then(setData)
       .catch(console.log)
     fetch("http://localhost:9003/userdata")
       .then((r) => r.json())
       .then(console.log)
-  }, [])
+  }, [place])
   console.log(data)
   let time = data.forecast ? data.current.last_updated_epoch : "";
 
+  function handleOnAutoClick(place){
+    setPlace(place)
+  }
+  function handleOnHamClick(){
+    setShowSideBar(show=>!show)
+  }
   return (
     <div style={{ display: "flex" }}>
-      <Settings />
+     {showSideBar?<Settings alerts={alerts} setAlerts={setAlerts}/>:null} 
       <div>
         <div>
-          <NavBar />
+         {data.location?<NavBar onHamClick={handleOnHamClick} showSideBar={showSideBar}  location={data.location} onAutoClick={handleOnAutoClick}/>:null} 
           {data.forecast ? (
             <div className="App">
               <Clouds
