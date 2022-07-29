fe9d79633f2958ac421b8b4f3836653502cad2f6
Aimkeys Kelvin Mwaura <kelvin.ngugi@student.moringaschool.com>
2022-07-29 09:10:58 +0300

    Edited settings side bar

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,4 +1,59 @@
-export default function Settings({handleSettingsChange}){
+export default function Settings({ handleSettingsChange }) {
     //is a child of navbar
     //switches on whenever you click or hover on the settings button
+    return (
+        <div className="settings">
+            <div className="avatars">
+                <img src="svg/wi-alien.svg" />
+            </div>
+            <p>Username</p>
+
+            <h3>Temperature</h3>
+            <div style={{display:"flex"}}>
+                  <p>Celcius</p>
+                <div>
+                    <label class="toggle-switch">
+                        <input id="temp-toggle" type="checkbox"/>
+                            <span class="slider-round"></span>
+                    </label>
+                </div>
+                
+            </div>
+            <div className="settings-line"></div>
+            <h3>Precipitation</h3>
+            <div style={{display:"flex"}}>
+                 <p>mm</p>
+                <div>
+                <label class="toggle-switch">
+                        <input id="temp-toggle" type="checkbox"/>
+                            <span class="slider-round"></span>
+                    </label>
+                </div>
+               
+            </div>
+            <div className="settings-line"></div>
+            <h3>Wind</h3>
+            <div style={{display:"flex"}}>
+               <p>km/h</p>
+                <div>
+                <label class="toggle-switch">
+                        <input id="temp-toggle" type="checkbox"/>
+                            <span class="slider-round"></span>
+                    </label>
+                </div>
+            </div>
+            <div style={{display:"flex"}}>
+                 <h3>Alerts</h3>
+            <select>
+                <option>Pollution</option>
+                <option>Floods</option>
+                <option>Rain</option>
+                <option>Snow</option>
+                <option>Tsunami</option>
+                <option>High Uv</option>
+            </select>
+            </div>
+           
+        </div>
+    )
 }
