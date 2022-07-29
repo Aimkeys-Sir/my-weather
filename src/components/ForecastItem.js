32aaab9debcb084d0cf58b20820d531c1dfc654c
Aimkeys Kelvin Mwaura <kelvin.ngugi@student.moringaschool.com>
2022-07-29 09:12:25 +0300

    Edit forecast items

--- src/components/ForecastItem.js
+++ src/components/ForecastItem.js
@@ -3,7 +3,7 @@ export default function ForecastItem({time,image,temp}){
         <div className="forecast-item">
             <h2>{time}</h2>
             <div className="fore-icon">
-            <img src={`svg/${image}.svg`} className="fore-image"/>
+            <img src={`svg/${image}.svg`}/>
             </div>
             <h4>{temp}&deg;</h4>
         </div>
