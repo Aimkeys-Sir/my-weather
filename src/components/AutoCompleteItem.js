c1810a378a82687b6aa6991640c36a679b431654
Aimkeys Kelvin Mwaura <kelvin.ngugi@student.moringaschool.com>
2022-07-29 15:40:39 +0300

    added component and functionality

---
+++ src/components/AutoCompleteItem.js
@@ -0,0 +1,15 @@
+export default function AutoCompleteItem({place,region,index,onAutoClick}) {
+    function handleAutoClick(){
+        onAutoClick(place)
+    }
+    return (
+        <div onClick={handleAutoClick} className="auto-div">
+            <div style={{display:"flex",padding:"4px"}}>
+                <h3>{place}</h3>
+            <p><em>{region}</em></p> 
+            </div>
+           
+            {index<9?(<div className="auto-complete-line"></div>):null}
+        </div>
+    )
+}
