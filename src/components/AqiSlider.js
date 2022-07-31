1c623425a5fcb18de07abd003b2bb515580c5615
Aimkeys Kelvin Mwaura <kelvin.ngugi@student.moringaschool.com>
2022-07-31 17:35:11 +0300

    component refactor

---
+++ src/components/AqiSlider.js
@@ -0,0 +1,23 @@
+export default function AqiSlider({ defra }) {
+    return (
+        <div>
+             <div className="defra-text">
+                <p>Good</p>
+                <p>Hazardous</p>
+            </div>
+            <div className="color-range">
+                <input type="range" min="0" max="10" value={defra} className="slider" id="myRange" />
+                <div className="one"></div>
+                <div className="two"></div>
+                <div className="three"></div>
+                <div className="four"></div>
+                <div className="five"></div>
+            </div>
+            <div className="defra-text">
+                <p>0</p>
+                <p>10</p>
+            </div>
+        </div>
+
+    )
+}
